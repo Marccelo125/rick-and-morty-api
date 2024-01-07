bcf6cf777ecad02a52913f72ff1a581925a3b9ac
Marcelo Duarte <Contato.marccelo125@gmail.com>
2024-01-07 14:09:24 -0300

    ⚙️undo: dark theme & modal concept

--- src/js/darkmode.js
+++
@@ -1,7 +0,0 @@
-const switchTheme = () => { 
-    const htmlElement = document.documentElement;
-    htmlElement.classList.toggle("light-theme");
-    htmlElement.classList.toggle("dark-theme");
-}
-
-document.getElementById("theme-switch").addEventListener("click", switchTheme);
